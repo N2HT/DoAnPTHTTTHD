26d2d159330910178c4d0543b8eb9ce3b2b24ee6
THANHNHAN <ntnahn@gmail.com>
2017-04-09 23:52:55 +0700

    1542250_SSIS

---
+++ BaiTap SSIS/1542250/1542250_SSIS/1542250_SSIS/DBScripts/TransactionProcessing.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE TransactionProcessing
+GO
+USE TransactionProcessing
+GO
+CREATE TABLE TransactionDetail
+(
+	RecordId INT PRIMARY KEY IDENTITY,
+	ReportDate DATE,
+	AccountNumber VARCHAR(100),
+	TransactionCode VARCHAR(1),
+	Amount MONEY,
+	MerchantNumber VARCHAR(100)
+)
+
+SELECT * FROM TransactionDetail
---
+++ BaiTap SSIS/1542250/1542250_SSIS/CallSSIS/Form1.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.SqlServer.Dts.Runtime;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CallSSIS {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void btnImport_Click(object sender, EventArgs e) {
+            String filePath = txtFilePath.Text;
+            if (filePath == "") {
+                MessageBox.Show("Please enter the data file path");
+                return;
+            }
+
+            // Call command line
+            string pkgLocation;
+            Package pkg;
+            Microsoft.SqlServer.Dts.Runtime.Application app;
+            DTSExecResult pkgResults;
+
+            pkgLocation =
+              @"..\..\..\1542250_SSIS\Transaction_Processing.dtsx";
+            app = new Microsoft.SqlServer.Dts.Runtime.Application();
+            pkg = app.LoadPackage(pkgLocation, null);
+            Variables appVars = pkg.Variables;
+            appVars["FileTransactionNamePath"].Value = "D:\transaction_lythuongkiet_01042017.dat";
+
+            pkgResults = pkg.Execute(null, appVars, null, null, null);
+
+            Console.WriteLine(pkgResults.ToString());
+            Console.ReadKey();
+        }
+    }
+}
