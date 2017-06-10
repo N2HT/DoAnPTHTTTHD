db2cdaf35dc3f2290fcc7964cf1853d1ebefacdb
ntttue <ntttue.gl@gmail.com>
2017-06-10 10:39:35 +0700

    Update structe JavaProject

--- JavaRestfulAPI/src/main/java/controller/UserController.java
+++ JavaRestfulAPI/src/main/java/controller/UserController.java
@@ -3,10 +3,8 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package hello;
+package controller;
 import model.Account;
-
-import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
